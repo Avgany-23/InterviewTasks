2389711ffd9f5aa26bb6e5ee5a5c90630f61a2fd
Neto nest <nike112200@yandex.ru>
2024-08-20 03:31:04 +0300

    update1

--- task1.py
+++ task1.py
@@ -19,23 +19,3 @@ class Stack:
 
     def size(self):
         return len(self.stack)
-
-
-brackets = ['(((([{}]))))', '[([])((([[[]]])))]{()}', '{{[()]}}', '}{}', '{{[(])]}}', '[[{())}]']
-
-
-def check_balanced_brackets(brackets):
-    dict_brackets = {']': '[',
-                     ')': '(',
-                     '}': '{'}
-    stack = Stack()
-    count = -1
-    count_brackets = len(brackets)
-    while (count := count + 1) < count_brackets:
-        add_element = brackets[count]
-        if add_element in dict_brackets:
-            if stack.pop() != dict_brackets[add_element]:
-                return False
-            continue
-        stack.push(add_element)
-    return stack.is_empty()
